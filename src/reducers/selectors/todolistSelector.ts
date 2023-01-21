f0ec3e119e04f8753ff9d6d68b3b8bd38e73eaa8
k2 <izmail.k2@gmail.com>
2023-01-21 19:05:56 +0300

    selectors

---
+++ src/reducers/selectors/todolistSelector.ts
@@ -0,0 +1,3 @@
+import {AppRootState} from "../store";
+
+export const todolistSelector = (state:AppRootState) => state.todoLists
--- src/reducers/store.ts
+++ src/reducers/store.ts
@@ -1,8 +1,14 @@
-import {combineReducers, legacy_createStore as createStore} from "redux";
+import {combineReducers, compose, legacy_createStore as createStore} from "redux";
 import {tasksReducer} from "./tasksReducer";
 import {todoListsReducer} from "./todoListsReducer";
 import {TypedUseSelectorHook, useSelector} from "react-redux";
 
+//помогает следить за нашим редаксковским стором
+declare global {
+    interface Window{
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+}
 
 
 const rootReducer = combineReducers({
@@ -10,8 +16,11 @@ const rootReducer = combineReducers({
     tasks: tasksReducer,
     })
 
+//функция нашего плагина для слежки за стором
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export type AppRootState = ReturnType<typeof rootReducer>;
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, composeEnhancers());
 export const useAppSelector: TypedUseSelectorHook<AppRootState> = useSelector
 
 
