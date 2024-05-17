dc54fa4540a361dfa2a006f7e53d176d25474a0f
k2 <izmail.k2@gmail.com>
2024-05-17 22:40:02 +0300

    refactor: Store now accepts grouped generators from different reducers

--- src/app/model/store.ts
+++ src/app/model/store.ts
@@ -1,16 +1,18 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { todoListsReducer } from "features/todolistsList/model/todos/todoListsReducer";
-import { tasksReducer } from "features/todolistsList/model/tasks/tasksReducer";
-import { appReducer, initAppSaga, INITIALIZED_APP } from "app/model/appReducer";
+import { sagasTasks, tasksReducer } from "features/todolistsList/model/tasks/tasksReducer";
+import { appReducer, sagasApp } from "app/model/appReducer";
 import { authReducer } from "features/auth/model/authReducer";
 import createSagaMiddleware from "redux-saga";
-import { takeEvery } from "redux-saga/effects";
+import { all } from "redux-saga/effects";
 
 const sagaMiddleware = createSagaMiddleware();
 
 function* rootSaga() {
-  yield takeEvery(INITIALIZED_APP, initAppSaga);
+  yield all([
+    sagasApp(),
+    sagasTasks()]);
 }
 
 export const rootReducer = combineReducers({
@@ -21,18 +23,12 @@ export const rootReducer = combineReducers({
 });
 export const store = configureStore({
   reducer: rootReducer,
-  // middleware: [sagaMiddleware],
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(sagaMiddleware),
 });
 
-sagaMiddleware.run(rootSaga); //подключаем и даем рутовый вотчер
+sagaMiddleware.run(rootSaga);
 
-// function* rootWatcher() {
-//   yield takeLatest(INITIALIZED_APP, initAppSaga); //принимает два параметра, экшн активатор и
-// }
-
-
-  export type AppDispatch = typeof store.dispatch;
+export type AppDispatch = typeof store.dispatch;
 
 export type AppRootStateType = ReturnType<typeof store.getState>;
 
