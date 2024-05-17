47418d7ea5d258913984797c61ac728487cedd0f
k2 <izmail.k2@gmail.com>
2024-05-17 22:44:33 +0300

    refactor: Now passing an action instead of a promise - Moved styles for btn into Button component

--- src/features/todolistsList/ui/todolist/Todolist.tsx
+++ src/features/todolistsList/ui/todolist/Todolist.tsx
@@ -3,12 +3,12 @@ import s from "features/todolistsList/ui/todolist/Todolist.module.css";
 import { FullInput } from "common/components/fullInputButton/FullInput";
 import { TodoAppType } from "features/todolistsList/model/todos/todoListsReducer";
 import { useAppSelector } from "app/model/store";
-import { tasksThunk } from "features/todolistsList/model/tasks/tasksReducer";
 import { statusSelector } from "features/todolistsList/model/todos/todolistSelector";
 import { useActions } from "common/hooks/useActions";
 import { FilterTasksBut } from "features/todolistsList/ui/todolist/filterTasksBut/FilterTasksBut";
 import { Tasks } from "features/todolistsList/ui/todolist/tasks/Tasks";
 import { TodoTitle } from "features/todolistsList/ui/todolist/todoTitle/TodoTitle";
+import { taskActions } from "features/todolistsList/model/tasks/tasksReducer";
 
 type TodolistProps = {
   todolist: TodoAppType;
@@ -16,34 +16,29 @@ type TodolistProps = {
 };
 
 export const Todolist = React.memo(({ demo = false, ...props }: TodolistProps) => {
-  console.log("Todolist");
 
-  const {id} = props.todolist;
+  const { id } = props.todolist;
 
   const status = useAppSelector(statusSelector);
 
-  const { addTasksTC, setTasksTC } = useActions(tasksThunk);
+  const { createTasksAction, requestTasksAction  } = useActions(taskActions);
 
   useEffect(() => {
-    setTasksTC(id)
+    requestTasksAction ({ todoId: id });
   }, []);
 
-  const addTask = useCallback(
-    (title: string) => {
-      return addTasksTC({ todoId: id, title: title });
-    },
-    [addTasksTC, id]
-  );
-
+  const addTask = useCallback((title: string) => {
+      return createTasksAction({ todoId: id, title: title });
+    }, [createTasksAction, id]);
 
   return (
     <>
-      <TodoTitle todolist={props.todolist}/>
+      <TodoTitle todolist={props.todolist} />
       <div className={s.block}>
         <FullInput addItem={addTask} disabled={status === "loading"} />
       </div>
       <ul>
-        <Tasks todolist={props.todolist}/>
+        <Tasks todolist={props.todolist} />
       </ul>
       <FilterTasksBut todo={props.todolist} />
     </>
