4258f0d1987776edc15877ee0a99249119cff94f
k2 <izmail.k2@gmail.com>
2024-06-26 17:05:52 +0300

    style: linked global style in index application

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "app/model/store";
 import AppRedux from "app/ui/AppRedux";
+import "assets/style/index.css";
 
-export const BASE_ROUT = '/it-incubator-todolist-ts-01'
+export const BASE_ROUT = "/it-incubator-todolist-ts-01";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-  root.render(
-    <BrowserRouter basename={BASE_ROUT}>
-      <Provider store={store}>
-        <AppRedux />
-      </Provider>
-    </BrowserRouter>
-  );
+root.render(
+  <BrowserRouter basename={BASE_ROUT}>
+    <Provider store={store}>
+      <AppRedux />
+    </Provider>
+  </BrowserRouter>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
