2c0a4b3bd648dbd238dac182b1a014aef1eea52b
k2 <izmail.k2@gmail.com>
2024-06-26 17:04:21 +0300

    refactor: add btn register to redirect to the registration page

--- src/common/components/buttonAppBar/ButtonAppBar.tsx
+++ src/common/components/buttonAppBar/ButtonAppBar.tsx
@@ -3,15 +3,15 @@ import { useCallback } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import AppRegistrationIcon from "@mui/icons-material/AppRegistration";
 import LinearProgress from "@mui/material/LinearProgress";
 import { ErrorSnackbar } from "common/components/errorSnackbar/ErrorSnackbar";
 import { StatusType } from "app/model/appReducer";
 import { authActions } from "features/auth/model/authReducer";
 import { useAppDispatch, useAppSelector } from "app/model/store";
+import { Typography } from "common/components/typographi/Typography";
+import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
 
 export function ButtonAppBar() {
   const status = useAppSelector<StatusType>((state) => state.app.status);
@@ -23,24 +23,29 @@ export function ButtonAppBar() {
   }, [dispatch]);
 
   return (
-    <Box sx={{ flexGrow: 1}}>
+    <Box sx={{ flexGrow: 1 }}>
       <ErrorSnackbar />
-      <AppBar position="static" style={{ backgroundColor: "brown", position: 'relative'}}>
-        <Toolbar>
-          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+      <AppBar position="sticky" color="inherit">
+        <Toolbar style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+          <Typography variant={"p"} style={{ textTransform: "uppercase", fontWeight: "900" }}>
+            Task Flow
           </Typography>
-          {isLoggedIn && (
+          {isLoggedIn ? (
             <Button color="inherit" onClick={logoutHandle}>
-              Log out
+              <MeetingRoomIcon />
+              <Typography variant={"p"}>Log out</Typography>
+            </Button>
+          ) : (
+            <Button color="inherit">
+              <AppRegistrationIcon />
+              <Typography variant={"a"} href={"https://social-network.samuraijs.com/"}>
+                register
+              </Typography>
             </Button>
           )}
         </Toolbar>
         {status === "loading" && (
-          <LinearProgress style={{ position: "absolute", left: "0", bottom: "-3px", height:'3px', width: '100%' }} />
+          <LinearProgress style={{ position: "absolute", left: "0", bottom: "-3px", height: "3px", width: "100%" }} />
         )}
       </AppBar>
     </Box>
