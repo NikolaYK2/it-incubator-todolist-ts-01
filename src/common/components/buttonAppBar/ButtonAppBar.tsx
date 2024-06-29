dffed2f5295975c201230ebbf8a1028315a0ba68
k2 <izmail.k2@gmail.com>
2024-06-29 22:37:11 +0300

    refactor: moved component ButtonAppBar.tsx

--- src/common/components/buttonAppBar/ButtonAppBar.tsx
+++ src/common/components/buttonAppBar/ButtonAppBar.tsx
@@ -7,42 +7,79 @@ import Button from "@mui/material/Button";
 import AppRegistrationIcon from "@mui/icons-material/AppRegistration";
 import LinearProgress from "@mui/material/LinearProgress";
 import { ErrorSnackbar } from "common/components/errorSnackbar/ErrorSnackbar";
-import { StatusType } from "app/model/appReducer";
 import { authActions } from "features/auth/model/authReducer";
 import { useAppDispatch, useAppSelector } from "app/model/store";
 import { Typography } from "common/components/typographi/Typography";
 import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
+import Grid from "@mui/material/Grid";
+import { FullInput } from "../fullInputButton/FullInput";
+import { statusSelector } from "../../../features/todolistsList/model/todos/todolistSelector";
+import { useActions } from "../../hooks/useActions";
+import { todoActions } from "../../../features/todolistsList/model/todos/todoListsReducer";
+import Container from "@mui/material/Container";
+import s from "./ButtonAppBar.module.css";
 
 export function ButtonAppBar() {
-  const status = useAppSelector<StatusType>((state) => state.app.status);
+  const status = useAppSelector(statusSelector);
   const isLoggedIn = useAppSelector<boolean>((state) => state.auth.isLoggedIn);
+
+  const { addTodoTitleAction } = useActions(todoActions);
+
   const dispatch = useAppDispatch();
 
   const logoutHandle = useCallback(() => {
     dispatch(authActions.authLogoutAction());
   }, [dispatch]);
 
+  const addTodolist = useCallback(
+    (title: string) => {
+      return addTodoTitleAction(title);
+    },
+    [addTodoTitleAction]
+  );
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <ErrorSnackbar />
-      <AppBar position="sticky" color="inherit">
-        <Toolbar style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
-          <Typography variant={"p"} style={{ textTransform: "uppercase", fontWeight: "900" }}>
-            Task Flow
-          </Typography>
-          {isLoggedIn ? (
-            <Button color="inherit" onClick={logoutHandle}>
-              <MeetingRoomIcon />
-              <Typography variant={"p"}>Log out</Typography>
-            </Button>
-          ) : (
-            <Button color="inherit">
-              <AppRegistrationIcon />
-              <Typography variant={"a"} href={"https://social-network.samuraijs.com/"}>
-                register
+      <AppBar
+        position="fixed"
+        color="inherit"
+        sx={{
+          boxShadow: "0 0 12px rgb(0 0 0 / 10%)",
+        }}
+      >
+        <Toolbar
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Container>
+            <Grid container display="flex" alignItems="center" justifyContent="space-between" padding={"10px"}>
+              <Typography variant={"p"} style={{ textTransform: "uppercase", fontWeight: "900" }}>
+                Task Flow
               </Typography>
-            </Button>
-          )}
+
+              <div className={s.input}>
+                <FullInput addItem={addTodolist} disabled={status === "loading"} />
+              </div>
+
+              {isLoggedIn ? (
+                <Button color="inherit" onClick={logoutHandle}>
+                  <MeetingRoomIcon />
+                  <Typography variant={"p"}>Log out</Typography>
+                </Button>
+              ) : (
+                <Button color="inherit">
+                  <AppRegistrationIcon />
+                  <Typography variant={"a"} href={"https://social-network.samuraijs.com/"}>
+                    register
+                  </Typography>
+                </Button>
+              )}
+            </Grid>
+          </Container>
         </Toolbar>
         {status === "loading" && (
           <LinearProgress style={{ position: "absolute", left: "0", bottom: "-3px", height: "3px", width: "100%" }} />
