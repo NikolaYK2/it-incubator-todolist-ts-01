a31f6580cd0c8b7f9dd059ae634448f9dad28954
k2 <izmail.k2@gmail.com>
2024-06-29 22:42:51 +0300

    feat: add animation for add task

--- src/features/todolistsList/ui/todolist/tasks/Tasks.tsx
+++ src/features/todolistsList/ui/todolist/tasks/Tasks.tsx
@@ -5,11 +5,13 @@ import { TaskStatuses } from "common/api/todolistsApi";
 import { Task } from "features/todolistsList/ui/todolist/tasks/task/Task";
 import { TodoAppType } from "features/todolistsList/model/todos/todoListsReducer";
 import s from "./Tasks.module.css";
+import { IconSvg } from "../../../../../common/components/iconSvg/IconSvg";
+import { m, LazyMotion, domAnimation, AnimatePresence } from "framer-motion";
 
-type Props={
-  todolist:TodoAppType;
-}
-export const Tasks = React.memo((props:Props) => {
+type Props = {
+  todolist: TodoAppType;
+};
+export const Tasks = React.memo((props: Props) => {
   const { id, filter } = props.todolist;
 
   const tasks = useAppSelector((state) => optimizedTaskSelect(state, id));
@@ -21,20 +23,38 @@ export const Tasks = React.memo((props:Props) => {
   if (filter === "Completed") {
     filterTasks = tasks.filter((t) => t.status === TaskStatuses.Completed);
   }
-
+  const taskAnimate = {
+    hidden: { x: 30, opacity: 0 },
+    visible: {
+      x: 0,
+      opacity: 1,
+    },
+    exit: { x: 30, opacity: 0 }, // анимация при удалении
+  };
   return (
-    <>
-      {tasks && tasks.length ? (
-        filterTasks.map((task) => {
-          return (
-            <li key={task.id} className={task.status ? s.activeTask : ""}>
-              <Task task={task} idTodolist={props.todolist.id} />
-            </li>
-          );
-        })
-      ) : (
-        <div className={s.tasksNull}>Task list is empty</div>
-      )}
-    </>
+    <LazyMotion features={domAnimation}>
+      <m.ul initial="hidden" animate="visible">
+        <AnimatePresence>
+          {tasks && tasks.length ? (
+            filterTasks.map((task) => (
+              <m.li
+                key={task.id}
+                className={task.status ? s.activeTask : ""}
+                variants={taskAnimate}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+              >
+                <Task task={task} idTodolist={props.todolist.id} />
+              </m.li>
+            ))
+          ) : (
+            <div className={s.tasksEmpty}>
+              <IconSvg iconName={"empty"} />
+            </div>
+          )}
+        </AnimatePresence>
+      </m.ul>
+    </LazyMotion>
   );
 });
